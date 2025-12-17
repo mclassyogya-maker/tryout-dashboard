4f4f0e99b672996864ba108f67169794c044bc38
mclassyogya-maker <mclassyogya@gmail.com>
2025-12-17 14:24:39 +0700

    Delete src/components/ui directory

--- src/components/ui/select-trigger.tsx
+++
@@ -1,5 +0,0 @@
-export interface SelectTriggerProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
-
-export default function SelectTrigger({ ...props }: SelectTriggerProps) {
-  return <button {...props} />
-}
--- src/components/ui/select-value.tsx
+++
@@ -1,5 +0,0 @@
-export interface SelectValueProps extends React.HTMLAttributes<HTMLSpanElement> {}
-
-export default function SelectValue({ ...props }: SelectValueProps) {
-  return <span {...props} />
-}
